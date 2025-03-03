f4047f64aa2da944f88ad72da42d19bd07dbea6c
Raunak Jodhawat <raunakjodhawat@gmail.com>
2025-03-03 14:55:30 +0530

    change trait

--- build.sbt
+++ build.sbt
@@ -4,5 +4,9 @@ ThisBuild / scalaVersion := "3.6.3"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "locksmith"
+    name := "locksmith-core",
+    organization := "com.raunakjodhawat"
   )
+
+// to load and override configuration from application.conf
+libraryDependencies += "com.github.pureconfig" %% "pureconfig-core" % "0.17.8"
--- src/main/scala/com/raunakjodhawat/locksmith/core/DistributedLock.scala
+++ src/main/scala/com/raunakjodhawat/locksmith/core/DistributedLock.scala
@@ -3,9 +3,5 @@ package com.raunakjodhawat.locksmith.core
 import scala.concurrent.duration.FiniteDuration
 
 trait DistributedLock {
-  def acquireLock(input: InputLockKey, duration: FiniteDuration): Boolean = {
-    val key = input.toHash
-    true
-  }
-  def releaseLock: Boolean
+  def lock(input: InputLockKey, duration: FiniteDuration): Boolean
 }
